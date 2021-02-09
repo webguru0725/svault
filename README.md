0bac92df32864537b7705f3ee9099d2d0db40890
webguru0725 <profes.sional0070@gmail.com>
2021-02-09 11:46:11 -0800

    initial

---
+++ README.md
@@ -0,0 +1,11 @@
+# GANACHE-CLI
+
+```bash
+ganache-cli --fork https://mainnet.infura.io/v3/$INFURA_KEY -p 7545
+```
+
+# TRUFFLE BASH
+
+```bash
+truffle test
+```
---
+++ migrations/2_token.js
@@ -0,0 +1,20 @@
+const Svault = artifacts.require("Svault");
+
+const migration = async (deployer, network, accounts) => {
+    await Promise.all([
+        deployToken(deployer, network),
+    ]);
+};
+
+module.exports = migration;
+
+async function deployToken(deployer, network) {
+    await deployer.deploy(Svault,
+            "0xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8",
+            "0xD7B7d3C0bdA57723Fb54ab95Fd8F9EA033AF37f2",
+            "0x0000000000000000000000000000000000000000",
+            "0x0000000000000000000000000000000000000000",
+            "Svault",
+            "0x2D2Ae0bfE25503f608f0f5cdCe54c17302C01Ea9",
+            "0xa0246c9032bC3A600820415aE600c6388619A14D");
+};
